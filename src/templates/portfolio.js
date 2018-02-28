6b56e699faeffff49f9d55a1e9ab0f64e9b44686
Rey Vera <veruxdev@gmail.com>
2018-02-27 20:35:26 -0800

    Edit the display on individual project pages.

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -7,19 +7,49 @@ import striptags from 'striptags'
 import { siteMetadata } from '../../gatsby-config'
 
 import SiteNavi from '../components/SiteNavi'
+import PortfolioNavi from '../components/PortfolioNavi'
 
 class PortfolioTemplate extends Component {
   render() {
+    const list = []
     const post = get(this, 'props.data.post')
     const site = get(this, 'props.data.site')
     const title = get(post, 'title.title')
     const siteTitle = get(site, 'meta.title')
+    const images = get(post, 'images')
+
+    console.log(images)
+
+    images.forEach((image, imgCount) => {
+      const imgLink = get(image, 'file.url')
+
+      list.push(
+        <div className="col-3 pt-5">
+          <img src={`https:${imgLink}`} className="img-fluid mx-auto d-block" />
+        </div>
+      )
+    })
 
     return (
       <div>
         <Helmet title={`${title} | ${siteTitle}`} />
         <SiteNavi title={siteMetadata.title} {...this.props} />
-        <h1>{title}</h1>
+        <div id="portfolio" className="container-fluid bg-even py-5">
+          <div id="portfolio-title" className="row justify-content-center">
+            <div className="col-7">
+              <p className="text-center display-4">{title}</p>
+            </div>
+          </div>
+          <PortfolioNavi
+            projects={this.props.data.portfolio.projects}
+            {...this.props}
+          />
+          <div className="row justify-content-center">
+            <div className="col-10">
+              <div className="row justify-content-center">{list}</div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -37,6 +67,29 @@ export const pageQuery = graphql`
     post: contentfulPhotoGallery(id: { eq: $id }) {
       title {
         title
+      }
+      images {
+        file {
+          url
+        }
+      }
+    }
+    portfolio: allContentfulPhotoGallery {
+      totalCount
+      projects: edges {
+        project: node {
+          id
+          categories
+          tags
+          title {
+            title
+          }
+          coverImage {
+            file {
+              url
+            }
+          }
+        }
       }
     }
   }
