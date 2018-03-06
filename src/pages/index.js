7f16ca95c37b812b1a20e1593bb7085a74a47e88
Rey Vera <veruxdev@gmail.com>
2018-03-06 10:10:26 -0800

    Test lazyload.

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,7 +65,7 @@ class Home extends Component {
             backgroundPosition: '-10em',
           }}
         >
-          <div className="row justify-content-center pt-7 pb-9">
+          <div className="row justify-content-center pt-7 pb-s-9 pb-16">
             <div className="col-lg-6 col-9">
               <img src={logo} className="img-fluid" />
             </div>
